879db22bccf531cc1b22712ec1837d2e0dbdbd43
Bernard Lyons <blyons22@msu.idserve.net>
2022-12-09 09:22:28 -0500

    Initial Commit

---
+++ Twitter 2/Twitter_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Twitter_2App.swift
+//  Twitter 2
+//
+//  Created by Bernard Lyons on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Twitter_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
